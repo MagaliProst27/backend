0ac26d8734a435dd5ca801456db12ee45142091f
27010-34-08 <27010-34-08@exchange.ad.afpanet>
2022-06-13 16:25:31 +0200

    PHP

---
+++ exercice_metis_PHP/index.php
@@ -0,0 +1,11 @@
+<body>
+
+<?php for ($i =3; $i<=7; $i++) 
+{
+echo '<font size"'.$i.'\>Hello World!</font><br />';
+echo '<font size"'.$i.'\>Hello World!</font><br />';
+echo '<font size"'.$i.'\>Hello World!</font><br />';
+}
+
+echo "<hr/>\n"?>;
+</body>
