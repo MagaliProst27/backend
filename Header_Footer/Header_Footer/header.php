c58aeeedc88dfc722cd2f0b59235d32ca54c9db4
27010-34-08 <27010-34-08@exchange.ad.afpanet>
2022-06-16 16:39:37 +0200

    menuiz

--- Header_Footer/Header_Footer/header.php
+++ Header_Footer/Header_Footer/header.php
@@ -3,12 +3,23 @@
 <?php session_start();
 if ((isset($_SESSION['login'])) || (!empty($_SESSION['login']))) 
 {$user=$_SESSION['login']; }
+if (isset($_SESSION["name"])) {
+    
+    
     ?>    <nav>
-            <h3><?php echo "Utilisateur : ".$user." "; ?> </h3>
-            <a href="index.php">Accueil</a>
+          
+            
            
-           
-           
+          <?php
+           echo '<a href="index.php">Accueil</a>';
+    echo '<br /><br /><a href="logout.php">Se déconnecter</a>';
+    } else {
+     
+    header("location:pdo_login.php");
+    }
+    
+    echo '<H1 class="form-legend">Bienvenue ' . $_SESSION["name"] . ' !</H1>';
+  ?>
             <img class="logo headLogo" src="img\MenuizMan_logo.png" alt="logo">
         </nav>
 </header>
--- Header_Footer/Header_Footer/index.php
+++ Header_Footer/Header_Footer/index.php
@@ -15,7 +15,7 @@
 
  //appel de la base de donnée
  echo "<main>";
-
+ 
 
         $mysqlConnection = new PDO('mysql:host=localhost;dbname=menuiz;charset=utf8', 'root', '');
         $produitStatement = $mysqlConnection->prepare('SELECT * FROM T_D_PRODUCT_PRD');
@@ -29,19 +29,23 @@
           <div class="card" id="product<?php echo $produit['PRD_ID'];?>">
                 <p><?php echo $produit['PRD_DESCRIPTION']; ?> </p>
                 <img src="https://youngplanneur.fr/wp-content/uploads/2018/09/portail-coulissant-aluminium-rioave-gris-l-350-cm-x-h-180-cm.jpg" alt="" >
-                <p> <?php echo $produit['PRD_GUARANTEE']; ?> mois</p>
+                <p> garantie : <?php echo $produit['PRD_GUARANTEE']; ?> mois</p>
                 <p><?php echo $produit['PRD_PRICE'];?> €</p>
                 <input type="text" class="product-quantity" name="quantity" value="1" size="2" />
-                <input type="submit" value= "Ajouter au panier" class="btnAddAction" />
+ <?php
+                echo '<a href="panier.php?action=ajout&amp;id_produit=' . $produit['PRD_ID'] . '&amp;quantite=1" 
+       onclick="window.open(this.href, '', 
+       'toolbar=no, location=no, directories=no, status=yes, scrollbars=yes, resizable=yes,
+        copyhistory=no, width=600, height=350'); return false;"
+          class="add-to-cart btn btn-primary">Ajouter au panier</a>';
+               ?>
                 </div>
         <?php
         }
-        
-
-
-
-
+       
+    //   
         echo "</main>";
+        include "panier.php";
     // FOOTER
      include "footer.php";
 ?>
